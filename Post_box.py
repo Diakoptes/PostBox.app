ff5203cb72034165d98a5d40edf2de07c5510248
Lukasz Kasprzyk <l.kasprzyk@protonmail.com>
2022-04-06 22:26:27 +0200

    changes and improvements

--- Post_box.py
+++ Post_box.py
@@ -5,79 +5,74 @@ class MailBox:
     :param owner_email: email used for notification
     """
     
-    def __init__(self, name, owner_email):
-        
-        self.name = name
-        self.quantity_letters = 80
+    def __init__(self, owner_email):
+        self.is_blocked = False # self.get_postbox_status("is_blocked")
+        self.letters_quantity = 0
+        self.max_quota = 100
         self.owner_email = owner_email
-        self.notification_type = ""
-        self.lock_check = ""
-        
-    """
-    Notifications from the app
-    """ 
     
-    def send_notification(self, notification_type) -> None:
-        """
-        Send notification to the owner with specific information
-        :param self:
-        :param notification_type: Type of notification eg. 
-            new_letter, _letter, _overload,brake_in
-        :return: None
-        """
-        if notification_type == "new_letter":
-            print(f"You have new letter in mailbox: {self.name}")
-            print(f"Email sent to {self.owner_email}")
-        elif notification_type == "_overload":
-            print(f"{self.name} is overload")
-            print(f"Email sent to {self.owner_email}")
-        elif notification_type == "brake_in":
-            print(f"breaking into the {self.name}")
-            print(f"Email sent to {self.owner_email}")
-            
-    def receive_letter(self) -> None:
-        """
-        MailBox send notification to user when it has got new letter
-        :param self:
-        :return: None
-        """
-        self.quantity_letters += 1
-        self.send_notification("new_letter")    
+    def voice_message(self, message):
+        """Informs postman about spam and overload"""
         
-    def mail_box_overload_condition(self) -> None:
+        return message
+
+    def is_spam(self, status):
+        """Checks whether the letter is on the spam list"""
+        
+        return status    
+
+    def send_notification(message):
+        """Sends notification about letters quantity and new letters."""
+        
+        return message
+
+    def new_letter(self):
         """
-        Notification about overload, max = 100 letters.
+        Informs postman about the postbox is blocked,
+        informs postman about a letter is spam,
+        sends notification to user and starts voice message to postman, about quota exceeded and postbox blocked,
         """
-    
-        if self.quantity_letters >= 80 and self.quantity_letters <= 99:
-            print(f"In your mailbox is only " + str(100-self.quantity_letters) + " slots.")
-            self.send_notification("_overload")   
-        elif self.quantity_letters == 100:
-            print("Your mailbox is full, post office can't deliver you any message!")
-            print(f"Email sent to {self.owner_email}")
-            self.send_notification("_overload")   
-        else:
+
+        if self.is_blocked == True:
+            self.voice_message("postbox is blocked")
+            self.send_notification("Letters quota is exceeded.") 
             pass
-    
-    def lock_(self):
-        """Mailbox is close."""
-        pass
-    
-    def open_(self):
-        """Mailbox is open."""
-        pass
-    
-    def lock_check(self) -> None:
-        """
-        Notification about unauthorized open.
-        """
-        pass
-    
+
+        if self.is_spam() == True:
+            self.voice_message("spam")
+            self.is_blocked = True
+            pass
+
+        if self.is_quota_exceeded() == True:
+            self.send_notification("Letters quota is exceeded.")
+            self.is_blocked = True
+            pass
+
+        if self.is_spam == False and self.is_quota_exceeded() == False:
+            self.is_blocked == False
+            self.quantity_letters += 1
+            self.send_notification("You have a new letter.")
+            self.is_quota_exceeded()
+            pass
+      
+    def is_quota_exceeded(self):
+        """Checks how many slots there is for letter."""
         
-    """
-    User funcionality
-    """
-    
-    def get_letter(self):
-        """when you pick up the letter from the mailbox"""
-        self.quantity_letters -= 1
+        if self.quantity_letters >= self.max_quota * 0.8:
+            self.send_notification("Postbox  almost full you have less than 20 percent.")
+            return False
+
+        elif self.quantity_letters > self.max_quota * 0.5:
+            self.send_notification("Your postbox is full more than fifty percent.")
+            return False
+        
+        elif self.quantity_letters == self.max_quota:
+            return True
+        else:
+            return False
+
+    def get_postbox_status(self, feature):
+        """In the future check GPIO pin and ask for block mechanism status"""
+        
+        if feature == "is _blocked":
+            return False
